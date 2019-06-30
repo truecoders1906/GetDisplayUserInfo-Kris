4fd1e2956ce22bae73dc29419adbcc3f5622d1a9
Kris <krisdixon@yahoo.com>
2019-06-30 13:36:42 -0500

    all items answered still needs work

--- GetDisplayUserInfo1/GetDisplayUserInfo1/Program.cs
+++ GetDisplayUserInfo1/GetDisplayUserInfo1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GetDisplayUserInfo1
 {
@@ -13,16 +14,44 @@ namespace GetDisplayUserInfo1
 
             Console.WriteLine("What is your first name?");
             firstName = Console.ReadLine();
+
             Console.WriteLine("What is your last name?");
             lastName = Console.ReadLine();
+
             Console.WriteLine("What is your favorite number?");
             favoriteNumber = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("How many pets do you have?");
             petCount = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Your name is " + firstName + " " + lastName);
-            Console.WriteLine("Your favorite number is " + favoriteNumber);
-            Console.WriteLine("You have " + petCount +" " + "pet(s)");
+
+
+            if (petCount != 0)
+            {
+
+                List<string> petName = new List<string>();
+                for (int i = 0; i < petCount; i++)
+                {
+                    Console.WriteLine("What are your pet(s) names? ", i + 1);
+                    petName.Add(Console.ReadLine());
+                }
+                Console.WriteLine("Your name is " + firstName + " " + lastName);
+                Console.WriteLine("Your favorite number is " + favoriteNumber);
+                Console.WriteLine("You have " + petCount + " " + "pet(s)");
+                Console.WriteLine("Your pet(s) names are:");
+                petName.ForEach(Console.WriteLine);
+
+            }
+            else
+            {
+                Console.WriteLine("Your name is " + firstName + " " + lastName);
+                Console.WriteLine("Your favorite number is " + favoriteNumber);
+                Console.WriteLine("You have " + petCount + " " + "pet(s)");
+            }
+
+
+            
+
         }
     }
 }
