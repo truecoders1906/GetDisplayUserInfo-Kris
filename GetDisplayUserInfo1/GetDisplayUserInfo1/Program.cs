f9820202d7622d53a65f331285d9adf6926e188e
Kris <krisdixon@yahoo.com>
2019-06-30 13:05:53 -0500

    2nd draft before pet count

--- GetDisplayUserInfo1/GetDisplayUserInfo1/Program.cs
+++ GetDisplayUserInfo1/GetDisplayUserInfo1/Program.cs
@@ -6,7 +6,23 @@ namespace GetDisplayUserInfo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName;
+            string lastName;
+            int favoriteNumber;
+            int petCount;
+
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+            Console.WriteLine("What is your favorite number?");
+            favoriteNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many pets do you have?");
+            petCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your name is " + firstName + " " + lastName);
+            Console.WriteLine("Your favorite number is " + favoriteNumber);
+            Console.WriteLine("You have " + petCount +" " + "pet(s)");
         }
     }
 }
