efe437f3f097eca57977fd5fde6cc2218ce1c270
Kris <krisdixon@yahoo.com>
2019-07-01 15:37:48 -0500

    2nd draft using a method

--- GetDisplayUserInfo1/GetDisplayUserInfo1/Program.cs
+++ GetDisplayUserInfo1/GetDisplayUserInfo1/Program.cs
@@ -5,50 +5,53 @@ namespace GetDisplayUserInfo1
 {
     class Program
     {        
-
-        static void getUserName()
+            static string getUserName(string question)
         {
-          Console.WriteLine("What is your first and last name?");
-        }
+            Console.WriteLine(question);
+            string answer = Console.ReadLine();
+            return answer;
+        }            
         static void Main(string[] args)
         {
-            string name;
-            int favoriteNumber;
-            int petCount;
-
-            getUserName();
-            name = Console.ReadLine();
-                       
+            Console.WriteLine("Hello, I would like some basic information from you.");
+            string firstName = getUserName("What is your first name?");
+            string lastName = getUserName("What is your last name?");                      
+                                                      
             Console.WriteLine("What is your favorite number?");
-            favoriteNumber = Convert.ToInt32(Console.ReadLine());
+            int favoriteNumber = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("How many pets do you have?");
-            petCount = Convert.ToInt32(Console.ReadLine());
+            int petCount = Convert.ToInt32(Console.ReadLine());
 
-            if (petCount != 0)
+            if (petCount > 1)
             {
-
                 List<string> petName = new List<string>();
                 for (int i = 0; i < petCount; i++)
                 {
                     Console.WriteLine("What is the name of your pet? ", i + 1);
                     petName.Add(Console.ReadLine());
                 }
-                Console.WriteLine("Your name is " + name + ".");
+                Console.WriteLine("Your name is " + firstName + " " + lastName + ".");
                 Console.WriteLine("Your favorite number is " + favoriteNumber);
                 Console.WriteLine("You have " + petCount + " " + "pet(s)");
-                Console.WriteLine("Your pet(s) names are:");
+                Console.WriteLine("Your pet's names are:");
                 petName.ForEach(Console.WriteLine);
-
             }
-            else
-            {
-                
-                Console.WriteLine("Your name is " + name + ".");
+            else if(petCount == 0)
+            {                
+                Console.WriteLine("Your name is " + firstName + " " + lastName + ".");
                 Console.WriteLine("Your favorite number is " + favoriteNumber);
-                Console.WriteLine("You have " + petCount + " " + "pet(s)");
+                Console.WriteLine("You do not have any pets.");
             }
+            else if(petCount == 1)
+            {
+                Console.WriteLine("What is the name of your pet?");
+                string petName = Console.ReadLine();
+                Console.WriteLine("Your name is " + firstName + " " + lastName + ".");
+                Console.WriteLine("Your favorite number is " + favoriteNumber + ".");
+                Console.WriteLine("Your pet's name is " + petName + ".");
 
+            }
             
             
 
